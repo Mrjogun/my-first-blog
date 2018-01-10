11b034732076046753617412914375bd61fdfda7
Mrjogun <audxo13@gmail.com>
2018-01-10 22:24:49 +0900

    Change bootstrap by W3layouts & Change about

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,15 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.home, name='home'),
+	url(r'^about/$', views.about, name='about'),
+	url(r'^courses/$', views.courses, name='courses'),
+	url(r'^join/$', views.join, name='join'),
+	url(r'^pages/icons/$', views.pages_icons, name='pages_icons'),
+	url(r'^pages/codes/$', views.pages_codes, name='pages_codes'),
+	url(r'^gallery/$', views.gallery, name='gallery'),
+	url(r'^contact/$', views.contact, name='contact'),
+	url(r'^login/$', views.login, name='login'),
+
 	url(r'^post/$', views.post_list, name='post_list'),
 	url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 	url(r'^post/new/$', views.post_new, name='post_new'),
--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,41 @@ from .forms import PostForm
 
 def home(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'bs/index.html', {'posts': posts})
+    return render(request, 'working/main.html', {'posts': posts})
+
+def about(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'working/about.html', {'posts': posts})
+
+def courses(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'working/courses.html', {'posts': posts})
+
+def join(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'working/join.html', {'posts': posts})
+
+def pages_icons(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'working/icons.html', {'posts': posts})
+
+def pages_codes(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'working/codes.html', {'posts': posts})
+
+def gallery(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'working/gallery.html', {'posts': posts})
+
+def contact(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'working/contact.html', {'posts': posts})
+
+def login(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'working/login.html', {'posts': posts})
+
+
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
